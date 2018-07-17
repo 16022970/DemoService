e67ce5979fe92b76181fae8ee36f9be53c7ee282
16022970 <16022970@myrp.edu.sg>
2018-07-17 17:37:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022970/demoservice/MyService.java
@@ -0,0 +1,15 @@
+package com.example.a16022970.demoservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
